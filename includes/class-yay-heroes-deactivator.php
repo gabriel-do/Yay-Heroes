c6c34fc05746d8ed982ccf7168520df120793f09
Gabriel Do <gabriel@yaycommerce.com>
2024-03-29 11:43:21 +0700

    first commit

---
+++ includes/class-yay-heroes-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://yaycommerce.com
+ * @since      1.0.0
+ *
+ * @package    Yay_Heroes
+ * @subpackage Yay_Heroes/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Yay_Heroes
+ * @subpackage Yay_Heroes/includes
+ * @author     Gabriel Do <gabriel@yaycommerce.com>
+ */
+class Yay_Heroes_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
